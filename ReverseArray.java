10b3b339779239ea9464090268d383cb846796be
Ivan Novakovic <151567212+eevon314@users.noreply.github.com>
2024-01-11 22:17:12 +0100

    Add files via upload

---
+++ ReverseArray.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class codilityTwoOne {
+    public static void main(String[] args) {
+        //Create an array
+        String[] numbers = {"one", "two", "three", "four", "five"};
+        //Reverse the array
+        for (int i = 0; i<Math.floor(numbers.length/2); i++) {
+            int len = numbers.length;
+            int k = len - i - 1;
+            String temp = numbers[i];
+            numbers[i] = numbers[k];
+            numbers[k] = temp;
+        }
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+
+    }
+
+    public int solution(List<String> shopping) {
+        return 0;
+    }
+
+}
