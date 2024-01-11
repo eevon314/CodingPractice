6b7c1f523eedbffccf9262bd45b5ea94aafe7192
Ivan Novakovic <151567212+eevon314@users.noreply.github.com>
2024-01-11 22:19:41 +0100

    Update and rename ReverseArray.java to reverseArray.java

--- reverseArray.java
+++ reverseArray.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class ReverseArray {
+public class reverseArray {
     public static void main(String[] args) {
         //Create an array
         String[] numbers = {"one", "two", "three", "four", "five"};
